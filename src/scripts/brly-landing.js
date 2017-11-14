71da2cff4225be1955e25f3e589b388ed2354bd8
Gricel Dayhanna Sepúlveda Rozas <gr3c2l@gmail.com>
2017-11-14 11:38:58 -0300

    final

--- src/scripts/brly-landing.js
+++ src/scripts/brly-landing.js
@@ -1,1 +1,40 @@
- 
+$(document).ready(function(){
+   // TRIGGER TOOL MENU
+   $("span.fa.fa-bars").click(function(){
+    if($(".brly-main-menu").hasClass("display"))
+        {
+          $(".brly-main-menu").removeClass("display");
+        }
+    else{
+          $(".brly-main-menu").addClass("display");
+        }
+  });
+  // TRIGGER TOOL MENU
+  $("span.fa.fa-sort-down").click(function(){
+    if($("ul.brly-logged-options").hasClass("display"))
+        {
+          $("ul.brly-logged-options").removeClass("display");
+        }
+    else{
+          $("ul.brly-logged-options").addClass("display");
+        }
+  });
+  //CLOSE POP
+  $("button.brly-register-close").click(function(){
+    $(this).closest(".brly-pop-wrapper").addClass("display-none");
+    $(".brly-bg-pop").addClass("display-none");
+  });
+  //CLOSE ALL POPS AND BACKGROUND POP
+  function closeAllPop(){
+    $(".brly-pop-wrapper").addClass("display-none");
+    $(".brly-bg-pop").addClass("display-none");
+  }
+  //CLOSE ALL POPS WITHOUT BACKGROUND POP
+  function closePop(){
+    $(".brly-pop-wrapper").addClass("display-none");
+  }
+  //CLOSE ONLY BACKGROUND POP
+  function closeBgPop(){
+    $(".brly-bg-pop").addClass("display-none");
+  }
+});
